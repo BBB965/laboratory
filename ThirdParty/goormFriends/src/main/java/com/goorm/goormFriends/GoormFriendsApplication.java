f4d7df87738c13c8849a2f9bd6ad3b423e1622b6
SPIDEY965 <bbnt25@gmail.com>
2023-12-08 14:22:41 +0900

    feat:github_social_login_setting

---
+++ ThirdParty/goormFriends/src/main/java/com/goorm/goormFriends/GoormFriendsApplication.java
@@ -0,0 +1,13 @@
+package com.goorm.goormFriends;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoormFriendsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoormFriendsApplication.class, args);
+	}
+
+}
---
+++ ThirdParty/goormFriends/src/test/java/com/goorm/goormFriends/GoormFriendsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goorm.goormFriends;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoormFriendsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
